e5745e6323d6bf128779ad5439116c4a20b4b6be
Everton Santos <everton_santos_01@hotmail.com>
2021-11-16 18:14:40 -0300

    Construtor "public Product(String name, double price, int quantity){}"

---
+++ src/application/AppProduct.java
@@ -0,0 +1,58 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitites.Product;
+
+public class AppProduct {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			System.out.println("Enter product data: ");
+			System.out.print("Name: ");
+			String name = sc.next();
+			
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			
+			System.out.print("Quantity in stock: ");
+			int quantity = sc.nextInt();
+			
+			Product product = new Product(name, price, quantity);
+			
+			System.out.println("\nProduct " + product);
+			
+			
+			System.out.print("\nEnter the number of products to be added in stock: ");
+			product.addProducts(sc.nextInt());
+			
+			System.out.println("\nUpdated " + product);
+			
+			System.out.print("\nEnter the number of products to be removed from stock: ");
+			product.removeProducts(sc.nextInt());
+			
+			System.out.println("\nUpdate " + product);
+			
+			System.out.println("\n*** Programa encerrado com sucesso! ***");
+				
+		} catch (NullPointerException n) {
+			System.err.println("Valor nulo");
+			n.printStackTrace();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
+		
+		
+
+	}
+
+}
